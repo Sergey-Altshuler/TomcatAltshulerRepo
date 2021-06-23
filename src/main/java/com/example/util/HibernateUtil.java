868532ff2e178216895145a9dd8c3c9370ac1921
SergeyAltshuler <altshuler-it@mail.ru>
2021-06-24 00:07:08 +0300

    default_commit

---
+++ src/main/java/com/example/util/HibernateUtil.java
@@ -0,0 +1,23 @@
+package com.example.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+    private static final EntityManagerFactory emFactory;
+
+    static {
+        emFactory = Persistence.createEntityManagerFactory("tomcat21_06_2021");
+    }
+
+    public static EntityManager getEntityManager() {
+        return emFactory.createEntityManager();
+    }
+
+    public static void close() {
+        if (emFactory != null) {
+            emFactory.close();
+        }
+    }
+}
